cd5df72ef33b081b7b5e59e538247719e7db2ca3
nepg82 <nepg82@gmail.com>
2026-08-07 18:16:16 -0400

    Use network-first service worker caching

--- sw.js
+++ sw.js
@@ -31,7 +31,17 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+        fetch(event.request)
+            .then(response => {
+
+                const responseClone = response.clone();
+
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseClone);
+                });
+
+                return response;
+            })
+            .catch(() => caches.match(event.request))
     );
 });
