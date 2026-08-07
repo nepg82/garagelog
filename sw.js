b052a18638bed9183a06ebb51f278fc2061df56b
nepg82 <nepg82@gmail.com>
2026-08-07 15:04:30 -0400

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "garage-log-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./css/style.css",
+    "./js/app.js",
+    "./data/GarageLog.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
